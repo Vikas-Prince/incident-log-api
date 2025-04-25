d0c2cfb92af2f169ba1758d15474c735da8b7b3d
Vikas-Prince <vikasprince30809@gmail.com>
2025-04-25 22:33:43 +0530

    chore(init): setup project structure with Express, MongoDB, and environment configs

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
+module.exports = app;
